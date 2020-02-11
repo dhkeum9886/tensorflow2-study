3206bd9a1b2574890200ff0a78a426857c2f0025
dhkeum <dhkeum9886@gmail.co.kr>
2020-02-11 09:28:16 +0900

    just commit

--- 2.review_text_categorize.py
+++ 2.review_text_categorize.py
@@ -12,12 +12,12 @@ import tensorflow_hub as hub
 import tensorflow_datasets as tfds
 
 
-print("버전: ", tfds.__file__)
+# print("버전: ", tfds.__file__)
 
-# print("버전: ", tf.__version__)
-# print("즉시 실행 모드: ", tf.executing_eagerly())
-# print("허브 버전: ", hub.__version__)
-# print("GPU ", "사용 가능" if tf.config.experimental.list_physical_devices("GPU") else "사용 불가능")
+print("버전: ", tf.__version__)
+print("즉시 실행 모드: ", tf.executing_eagerly())
+print("허브 버전: ", hub.__version__)
+print("GPU ", "사용 가능" if tf.config.experimental.list_physical_devices("GPU") else "사용 불가능")
 
 
 
@@ -28,16 +28,9 @@ if __name__ == '__main__':
 
     # IMDB 데이터셋 다운로드
     train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
-    #
+    # # 아래 코드에서 에러 발생함.
+    # # 원인 파악하기 귀찮아서..안함.
     # (train_data, validation_data), test_data = tfds.load(
     #     name="imdb_reviews",
     #     split=(train_validation_split, tfds.Split.TEST),
     #     as_supervised=True)
-
-    # try:
-    #     (train_data, validation_data), test_data = tfds.load(
-    #         name="imdb_reviews",
-    #         split=(train_validation_split, tfds.Split.TEST),
-    #         as_supervised=True)
-    # except  AssertionError as ae:
-    #     print(ae)
